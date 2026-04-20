64cb27da345c8052d4c96d896b7855a40f18cf1a
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2026-04-20 21:44:03 +0900

    Forward Proxyガチアンチワイ、魑魅魍魎が跋扈するCloudflare Free Tierだけは泣く泣くWARP Connector SWGで検閲する
    NAT64のPMTUでECHが剝がれて、network policiesで評価できるのがポイント

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 server=nat64.fro
-daemons="radvd ndppd tayga@pp01 tayga@pp03 tayga@tu00 tayga@tu10 tayga@tu20 tayga@warp tayga@warp2"
+daemons="radvd ndppd tayga@pp01 tayga@pp03 tayga@tu00 tayga@tu10 tayga@tu20 tayga@warp tayga@warp2 tayga@warp3030 tayga@warp3031 tayga@warp3032 tayga@warp3033 tayga@warp3034 tayga@warp3035 tayga@warp3036 tayga@warp3037"
 
 rsync -rtv \
   --exclude='.git/' \
